551e7769a695df6cd85639f5ed741cdd0037bf54
ShihaoJing <shihao.jing810@gmail.com>
2017-06-29 16:07:57 -0400

    use Trie routing in Router

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -45,34 +45,34 @@ TEST(Routing, SimplePath) {
 
 TEST(Routing, Trie) {
   {
-    TrieNode *root = new TrieNode;
+    Trie *trie = new Trie;
 
     vector<string> keys = {"/", "/hello", "/id/<int>"};
     for (auto key : keys) {
-      insert(root, key);
+      trie->insert(key);
     }
 
-    EXPECT_TRUE(search(root, "/"));
-    EXPECT_TRUE(search(root, "/hello"));
-    EXPECT_TRUE(search(root, "/id/123"));
-    EXPECT_TRUE(search(root, "/id/+123"));
-    EXPECT_TRUE(search(root, "/id/-123"));
-    EXPECT_FALSE(search(root, "/hello/12a"));
+    EXPECT_TRUE(trie->search("/"));
+    EXPECT_TRUE(trie->search("/hello"));
+    EXPECT_TRUE(trie->search("/id/123"));
+    EXPECT_TRUE(trie->search("/id/+123"));
+    EXPECT_TRUE(trie->search("/id/-123"));
+    EXPECT_FALSE(trie->search("/hello/12a"));
   }
 
   {
-    TrieNode *root = new TrieNode;
+    Trie *trie = new Trie;
 
     vector<string> keys = {"/hello/", "/id/<int>/"};
     for (auto key : keys) {
-      insert(root, key);
+      trie->insert(key);
     }
 
-    EXPECT_TRUE(search(root, "/id/123/"));
+    EXPECT_TRUE(trie->search("/id/123/"));
     // below examples need to be redirected with trailing slash to make TRUE
-    EXPECT_FALSE(search(root, "/hello"));
-    EXPECT_FALSE(search(root, "/id/123"));
-    EXPECT_FALSE(search(root, "/id/+123"));
-    EXPECT_FALSE(search(root, "/id/-123"));
+    EXPECT_FALSE(trie->search("/hello"));
+    EXPECT_FALSE(trie->search("/id/123"));
+    EXPECT_FALSE(trie->search("/id/+123"));
+    EXPECT_FALSE(trie->search("/id/-123"));
   }
 }
--- zion/routing.h
+++ zion/routing.h
@@ -15,115 +15,9 @@
 
 namespace zion {
 
-enum class ParamType
-{
-  INT
-};
-
-struct ParamTraits
-{
-  ParamType type;
-  std::string name;
-};
-
-class Rule;
-
-struct TrieNode
-{
-  TrieNode *children[ALPHABET_SIZE] = {};
-  TrieNode *param_children[PARAMTYPE_NUM] = {};
-
-  std::unique_ptr<Rule> rule;
-  bool isLeaf = false;
-};
-
-void insert(TrieNode *root, std::string key) {
-  TrieNode *cur = root;
-  for (int i = 0; i < key.length(); ++i) {
-    char c = key[i];
-    if (c == '<') {
-      bool found = false;
-
-      static ParamTraits paramTraits[] =
-          {
-              { ParamType::INT, "<int>" },
-          };
-
-      for(auto it = std::begin(paramTraits); it != std::end(paramTraits); ++it)
-      {
-        if (key.compare(i, it->name.size(), it->name) == 0)
-        {
-          if (!cur->param_children[(int)it->type]) {
-            cur->param_children[(int)it->type] = new TrieNode;
-          }
-          cur = cur->param_children[(int)it->type];
-          i += it->name.size() - 1; // for loop will increment i by 1
-          found = true;
-          break;
-        }
-      }
-      if (!found) {
-        throw std::runtime_error("Invalid parameter type" + key);
-      }
-    }
-    else {
-      int index = key[i] - '!';
-      if (!cur->children[index]) {
-        cur->children[index] = new TrieNode;
-      }
-      cur = cur->children[index];
-    }
-  }
-  if (cur != root) cur->isLeaf = true;
-}
-
-bool search(TrieNode *root, std::string key) {
-  TrieNode *cur = root;
-  for (size_t i = 0; i < key.length(); /* */) {
-    char c = key[i];
-    if (c == '/') {
-      int index = key[i] - '!';
-      if (!cur->children[index])
-        return false;
-      cur = cur->children[index];
-      ++i;
-    }
-    else {
-      // match <int> pattern
-      if (cur->param_children[0]) {
-        long long value = 0;
-        int type = 1;
-        int j = i;
-        while (j < key.length()) {
-          if ( j != i && (key[j] < '0' || key[j] > '9'))
-            break;
-          if (key[j] == '-') type = -1;
-          value = value * 10 + key[j] - '0';
-          ++j;
-        }
-        if (j == key.length() || key[j] == '/') {
-          i = j;
-          cur = cur->param_children[0];
-          continue;
-        }
-      }
-
-      // no param pattern match, do normal path matching
-      while (i < key.length() && key[i] != '/') {
-        int index = key[i] - '!';
-        if (!cur->children[index])
-          return false;
-        cur = cur->children[index];
-        ++i;
-      }
-    }
-  }
-  return cur != nullptr && cur->isLeaf;
-}
-
 class Rule
 {
-public:
+ public:
   Rule(std::string rule) : rule_(rule){
 
   }
@@ -143,37 +37,153 @@ public:
     return handler_();
   }
 
-private:
+ private:
   std::string rule_;
   std::string name_;
   std::function<response()> handler_;
 };
 
+class Trie
+{
+  struct TrieNode
+  {
+    TrieNode *children[ALPHABET_SIZE] = {};
+    TrieNode *param_children[PARAMTYPE_NUM] = {};
+
+    Rule *rule = nullptr;
+    bool isLeaf = false;
+  };
+
+  enum class ParamType
+  {
+    INT
+  };
+
+  struct ParamTraits
+  {
+    ParamType type;
+    std::string name;
+  };
+
+public:
+  Trie() : root_(new TrieNode)
+  {
+  }
+
+  Rule& insert(std::string key) {
+    TrieNode *cur = root_;
+    for (int i = 0; i < key.length(); ++i) {
+      char c = key[i];
+      if (c == '<') {
+        bool found = false;
+
+        static ParamTraits paramTraits[] =
+            {
+                { ParamType::INT, "<int>" },
+            };
+
+        for(auto it = std::begin(paramTraits); it != std::end(paramTraits); ++it)
+        {
+          if (key.compare(i, it->name.size(), it->name) == 0)
+          {
+            if (!cur->param_children[(int)it->type]) {
+              cur->param_children[(int)it->type] = new TrieNode;
+            }
+            cur = cur->param_children[(int)it->type];
+            i += it->name.size() - 1; // for loop will increment i by 1
+            found = true;
+            break;
+          }
+        }
+        if (!found) {
+          throw std::runtime_error("Invalid parameter type" + key);
+        }
+      }
+      else {
+        int index = key[i] - '!';
+        if (!cur->children[index]) {
+          cur->children[index] = new TrieNode;
+        }
+        cur = cur->children[index];
+      }
+    }
+    cur->rule = new Rule(key);
+    cur->isLeaf = true;
+    return *(cur->rule);
+  }
+
+  Rule* search(std::string key) {
+    TrieNode *cur = root_;
+    for (size_t i = 0; i < key.length(); /* */) {
+      char c = key[i];
+      if (c == '/') {
+        int index = key[i] - '!';
+        if (!cur->children[index])
+          return nullptr;
+        cur = cur->children[index];
+        ++i;
+      }
+      else {
+        // match <int> pattern
+        if (cur->param_children[0]) {
+          long long value = 0;
+          int type = 1;
+          int j = i;
+          while (j < key.length()) {
+            if ( j != i && (key[j] < '0' || key[j] > '9'))
+              break;
+            if (key[j] == '-') type = -1;
+            value = value * 10 + key[j] - '0';
+            ++j;
+          }
+          if (j == key.length() || key[j] == '/') {
+            i = j;
+            cur = cur->param_children[0];
+            continue;
+          }
+        }
+
+        // no param pattern match, do normal path matching
+        while (i < key.length() && key[i] != '/') {
+          int index = key[i] - '!';
+          if (!cur->children[index])
+            return nullptr;
+          cur = cur->children[index];
+          ++i;
+        }
+      }
+    }
+    if (cur == nullptr || cur->rule == nullptr)
+      return nullptr;
+    else return cur->rule;
+  }
+
+private:
+  TrieNode *root_;
+};
+
 class Router
 {
 public:
-  Router()
+  Router() : trie_()
   {
   }
 
   Rule& new_rule(std::string &rule) {
-    rules_.push_back(Rule(rule));
-    return rules_.back();
+    return trie_.insert(rule);
   }
 
   response handle(const request &req)
   {
-    for (auto rule: rules_) {
-      if (rule.match(req)) {
-        return rule.handle();
-      }
-    }
+    Rule *rule = trie_.search(req.uri);
+    if (rule)
+      return rule->handle();
 
     return response(response::not_found);
   }
 
 private:
-  std::vector<Rule> rules_;
+  Trie trie_;
 };
 
 }
